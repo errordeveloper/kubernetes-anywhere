7e3905fb04e7498b6d7d919e8bb3d7bd8a5d83bd
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-07-15 14:24:36 +0100

    Cleanup some logs as well

--- phase2/kube-alpha/cleanup.sh
+++ phase2/kube-alpha/cleanup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 docker-machine ssh vm1 'docker ps -a -q | xargs -r docker rm -f'
+docker-machine ssh vm1 'rm *.log'
 docker-machine ssh vm2 'docker ps -a -q | xargs -r docker rm -f'
+docker-machine ssh vm2 'rm *.log'
