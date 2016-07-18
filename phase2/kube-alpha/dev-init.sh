1d2fa0777b2602fca68b3d3c344b6decd15c4f61
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-07-18 12:03:28 +0100

    Make sure we get Docker 1.11 with VMWare as well

--- phase2/kube-alpha/dev-init.sh
+++ phase2/kube-alpha/dev-init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export VIRTUALBOX_BOOT2DOCKER_URL="https://github.com/boot2docker/boot2docker/releases/download/v1.11.2/boot2docker.iso"
+export boot2docker_url="https://github.com/boot2docker/boot2docker/releases/download/v1.11.2/boot2docker.iso"
+export VIRTUALBOX_BOOT2DOCKER_URL="${boot2docker_url}" FUSION_BOOT2DOCKER_URL="${boot2docker_url}"
 docker-machine create -d "${DOCKER_MACHINE_DRIVER:-virtualbox}" vm1
 docker-machine create -d "${DOCKER_MACHINE_DRIVER:-virtualbox}" vm2
