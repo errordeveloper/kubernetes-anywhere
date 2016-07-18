19d9ea390547b6b5e1716d1b184b2ba3cf7c893b
Luke Marsden <luke@clusterhq.com>
2016-07-18 11:39:38 +0100

    Have an off-by-default auto mode

--- phase2/kube-alpha/dev.sh
+++ phase2/kube-alpha/dev.sh
@@ -7,5 +7,12 @@ IP2=`docker-machine ip vm2`
 docker-machine scp ./kube vm1:/tmp/kube
 docker-machine scp ./kube vm2:/tmp/kube
 
-docker-machine ssh vm1 sudo /tmp/kube init "${IP1},${IP2}" &
-docker-machine ssh vm2 sudo /tmp/kube join "${IP1},${IP2}"
+if [ "$1" = "auto" ]; then
+    docker-machine ssh vm1 sudo /tmp/kube init "${IP1},${IP2}" &
+    pid="$$"
+    docker-machine ssh vm2 sudo /tmp/kube join "${IP1},${IP2}"
+    wait "$pid" 2>/dev/null
+else
+    echo "Log into vm1 with 'docker-machine ssh vm1' and run '/tmp/kube init ${IP1},${IP2}"
+    echo "Also, log into vm2 with 'docker-machine ssh vm2' and run '/tmp/kube join ${IP1},${IP2}'"
+fi
