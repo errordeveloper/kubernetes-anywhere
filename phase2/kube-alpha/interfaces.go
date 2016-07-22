9a49c5189147fc2c3cb6a693832a822461bcf38b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-07-22 14:50:54 +0100

    Revert "Reduce some duplication in the new interface file."
    This reverts commit d31d3c643a61fe575e3d5a374a19c4ac0208c945.

--- phase2/kube-alpha/interfaces.go
+++ phase2/kube-alpha/interfaces.go
@@ -1,8 +1,8 @@
 package main
 
 type ClusterInfo struct {
-	NodeIPs   []string
-	MasterIPs []string
+	nodeIPs   []string
+	masterIPs []string
 	// we will also need a field for DNS name(s)
 	// or any static public IP, whatever is going
 	// to be used to access the API server externally
@@ -12,7 +12,7 @@ type ClusterInfo struct {
 // - there is definetly a need for the idea of 'FirstMaster',
 //   all the some implementations might not have to use it
 
-// this is top-level interface, right now it's here for sanity
+// this is top-level interface, rigth now it's here for sanity
 // perhaps we should actually have something like this and use
 // to package collections of implementations, as probably very
 // few of the implementations will work well together, or even
@@ -38,26 +38,32 @@ type NodeRendezvousProvider interface {
 	Launch()
 }
 
-// For PKI, on one special node we initialize the PKI material and make it
-// available to the other nodes (somehow); on all other nodes we just fetch
-// that material and then install it.
 type FirstMasterPKI interface {
 	Init()
 	Publish()
 }
 
-type AnyServerPKI interface {
-    // XXX is there information conveyed in the type of server (master vs node)
-    // we run this on?
+type AnyMasterPKI interface {
 	Fetch()
-	Install()
+	Init()
 }
 
-// Pod network setup involves installing a CNI plugin, and then doing any
-// backend-specific launch actions.
-type PodNetworkProvider interface {
-    // XXX is there information conveyed in the type of server (master vs node)
-    // we run this on?
-	Install()
+type NodePKI interface {
+	Fetch()
+	Init()
+}
+
+type FirstMasterNetworkProvider interface {
+	Init()
+	Launch()
+}
+
+type AnyMasterNetworkProvider interface {
+	Init()
+	Launch()
+}
+
+type NodeNetworkProvider interface {
+	Init()
 	Launch()
 }
